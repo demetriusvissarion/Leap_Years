e1c71ab948aa603f9b17c6f9ca6105de9766f057
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-02-15 11:36:53 +0000

    finished view

--- Leap_Years/ContentView.swift
+++ Leap_Years/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputYear = ""
+    @State private var leapYearResults = [String]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Leap Year Checker App")
+                .font(.largeTitle)
+            
+            Image("homepage1")
+                .resizable()
+                .frame(width: .infinity, height: 200.0)
+            
+            HStack {
+                TextField("Add a year to check", text: $inputYear)
+                    .padding()
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Button("Check") {
+                    checkLeapYear()
+                }
+                .padding(8)
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            }
+            .padding(.trailing, 30)
+            List(leapYearResults, id: \.self) { result in
+                Text(result)
+            }
+            
+            
+            Spacer()
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.blue.opacity(0.7))
+        .padding(.horizontal, 0)
+        .padding(.vertical, 0)
+        
+        HStack {
+            
+        }
+    }
+    
+    private func checkLeapYear() {
+        guard let year = Int(inputYear) else {
+            prependResult("Please enter a valid year.")
+            return
+        }
+        
+        let leapYears = Leap_Years()
+        if leapYears.checkIfLeapYear(year: year) {
+            prependResult("\(year) is a leap year.")
+        } else {
+            prependResult("\(year) is NOT a leap year.")
+        }
+    }
+    
+    private func prependResult(_ result: String) {
+        leapYearResults.insert(result, at: 0)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
 
