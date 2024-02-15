38d12dfd6b1574fee2423ad1b525eda8c11de85a
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-02-15 10:37:08 +0000

    finished TDD main function

--- Leap_Years/Leap_Years.swift
+++ Leap_Years/Leap_Years.swift
@@ -7,6 +7,25 @@
 
 import Foundation
 
-private func leap_year(year: Int) {
+class Leap_Years {
+    
+    func checkIfLeapYear(year: Int) -> Bool {
+        if year % 400 == 0
+        {
+            return true
+        } else if year % 4 == 0 && year % 100 != 0
+        {
+            return true
+        } else
+        {
+            return false
+        }
+    }
     
 }
+
+//All years divisible by 400 ARE leap years (e.g. 2000 was a leap year)
+//All years divisible by 4 and not by 100 ARE leap years (e.g. 2004, 2008 and 2012 were leap years)
+
+//All years divisible by 100 but not by 400 are NOT leap years (e.g. 1700, 1800 and 1900 were not leap years)
+//All years not divisible by 4 are NOT leap years (e.g. 2009, 2010 and 2011 were not leap years)
--- Leap_YearsTests/Leap_YearsTests.swift
+++ Leap_YearsTests/Leap_YearsTests.swift
@@ -10,27 +10,48 @@ import XCTest
 
 final class Leap_YearsTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    // Test behaviour of leap_year func
+    func testLeap_Years() {
+        // Arrange
+        let leap_year = Leap_Years()
+        
+        // Act
+        let result1 = leap_year.checkIfLeapYear(year: 2020)
+        let result2 = leap_year.checkIfLeapYear(year: 1970)
+        let result3 = leap_year.checkIfLeapYear(year: 1900)
+        let result4 = leap_year.checkIfLeapYear(year: 1988)
+        let result5 = leap_year.checkIfLeapYear(year: 1500)
+        
+        // Assert
+        XCTAssertTrue(result1) // should return true
+        XCTAssertFalse(result2) // sould return false
+        XCTAssertFalse(result3) // sould return false
+        XCTAssertTrue(result4) // sould return true
+        XCTAssertFalse(result5) // sould return false
     }
 
 }
+
+
+//override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+//
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//        // Any test you write for XCTest can be annotated as throws and async.
+//        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+//        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
